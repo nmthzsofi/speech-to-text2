a39e9153b6306cfd66cbccf8d82ab1de36c0cec3
nmthzsofi <168550167+nmthzsofi@users.noreply.github.com>
2024-11-21 21:19:07 +0100

    Update server.js

--- server.js
+++ server.js
@@ -62,6 +62,6 @@ app.post('/save-all-answers', (req, res) => {
     });
 });
 
-app.listen(3000, () => {
-    console.log('Server started on port 3000');
+app.listen( process.env.PORT || 3000, () => {
+    console.log('Server started on port!');
 });
