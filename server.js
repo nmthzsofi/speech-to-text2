0d055e50f8ece73cbffed40539fdb9fe99ea5015
nmthzsofi <168550167+nmthzsofi@users.noreply.github.com>
2024-11-22 17:59:48 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-/*const express = require('express');
+const express = require('express');
 const multer = require('multer');
 const { SpeechClient } = require('@google-cloud/speech');
 const fs = require('fs');
@@ -71,22 +71,3 @@ app.listen(port, () => {
     console.log('Server started on port!');
 });
 console.log("server started");
-*/
-
-const express = require('express');
-const app = express();
-
-const port = process.env.PORT || 3000;
-
-console.log("Server starting...");
-console.log("Port:", port);
-
-app.get('/', (req, res) => {
-    res.send('Hello World');
-});
-
-app.listen(port, () => {
-    console.log(`Server is listening on port ${port}`);
-});
-
-console.log("Server set!");
